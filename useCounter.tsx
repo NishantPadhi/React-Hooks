c6c89e9f3aed0e2df02d94fd0a4545ddce6fd715
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-03 10:00:44 +0530

    added useCounter hook

---
+++ useCounter.tsx
@@ -0,0 +1,17 @@
+interface UseCounterReturn {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+  reset: () => void;
+  setCount: Dispatch<SetStateAction<number>>;
+}
+
+export default function useCounter(initialValue?: number): UseCounterReturn {
+  const [count, setCount] = useState(initialValue ?? 0);
+
+  const increment = () => setCount(prevCount => prevCount + 1);
+  const decrement = () => setCount(prevCount => prevCount - 1);
+  const reset = () => setCount(initialValue ?? 0);
+
+  return { count, increment, decrement, reset, setCount };
+}

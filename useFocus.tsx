f91a6a4da87823c42e987fa493255f207e7b87ac
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-05 18:58:56 +0530

    added useFocus hook

---
+++ useFocus.tsx
@@ -0,0 +1,17 @@
+// Implement a useFocus hook that enables programmatic focusing of an element.
+import { RefObject, useCallback, useRef } from 'react';
+
+export default function useFocus<T extends HTMLElement>(): [
+  RefObject<T>,
+  () => void,
+] {
+  const ref = useRef<T>(null);
+
+  const focusElement = useCallback(() => {
+    requestAnimationFrame(() => {
+      ref.current?.focus();
+    });
+  }, []);
+
+  return [ref, focusElement];
+}

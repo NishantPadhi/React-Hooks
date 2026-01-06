9eb492d5bdb29cb8738815e1b21e54c90c8fac29
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-06 19:17:31 +0530

    added useInterval hook

---
+++ useInterval.tsx
@@ -0,0 +1,27 @@
+// Implement a useInterval hook that creates an interval that invokes a callback function at a specified delay.
+import { useEffect, useRef } from 'react';
+
+export default function useInterval(
+  callback: () => void,
+  delay: number | null,
+) {
+  const savedCallback = useRef(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (delay === null) {
+      return;
+    }
+
+    const id = setInterval(() => {
+      savedCallback.current();
+    }, delay);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [delay]);
+}

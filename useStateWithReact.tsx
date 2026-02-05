16c3ae35feb60640d3bc8918f7866427137d8a3d
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-05 16:19:43 +0530

    added description

---
+++ useStateWithReact.tsx
@@ -0,0 +1,11 @@
+/**
+ * Implement a useStateWithReset hook that's similar to useState but with an 
+ * additional reset function that resets the state to its initial value.
+ * 
+ * Arguments
+ * initialValue: the initial value of the state. 
+ * This argument should be the same as the first argument of the useState hook
+ * Returns
+ * The useStateWithReset hook should have the same return values as the useState hook, 
+ * plus an additional function that resets the state to initialValue
+*/

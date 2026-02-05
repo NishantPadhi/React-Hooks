69102fed93c1e3ec39316aa53863c3e33cbcd7e0
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-05 16:20:34 +0530

    added solution

--- useStateWithReact.tsx
+++ useStateWithReact.tsx
@@ -24,3 +24,25 @@ export default function Component() {
   );
 }
 */
+
+import { useCallback, useMemo, useState } from 'react';
+
+export default function useStateWithReset(initialStateOrInitializer) {
+  const initialState = useMemo(() => {
+    if (
+      typeof initialStateOrInitializer === 'function' &&
+      initialStateOrInitializer.length === 0
+    )
+      return initialStateOrInitializer();
+
+    return initialStateOrInitializer;
+  }, []);
+
+  const [state, setState] = useState(initialState);
+
+  const reset = useCallback(() => {
+    setState(initialState);
+  }, [initialState]);
+
+  return [state, setState, reset];
+}
